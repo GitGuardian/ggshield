fa0bda9a3065765098972dbc167a016899f93d0d
Faramad Seifi <faramad.seifi@gitguardian.com>
2019-05-10 16:20:01 +0200

    Add setup configuration

--- secrets_shield/secrets_shield.py
+++ secrets_shield/secrets_shield.py
@@ -7,13 +7,13 @@ import subprocess
 
 import json
 from typing import Dict, Union
+import re
 import requests
 
 
 class ScanningApiClient:
     VERSION = "v2"
     DEFAULT_BASE_URL = "https://scanning.api.dev.gitguardian.com"
-    COMMIT_ROUTE = "/{}/scan/github/commit".format(VERSION)
     FILE_ROUTE = "/{}/scan/file".format(VERSION)
     TIMEOUT = 10
 
@@ -31,6 +31,9 @@ class ScanningApiClient:
     def _scan_file(
         self, content: str, filename: str = None, check: Union[bool, None] = None
     ) -> requests.Response:
+        """
+            Calls Scanning API and returns response
+        """
         payload = {"content": content}
         if filename:
             payload["filename"] = filename
@@ -45,7 +48,10 @@ class ScanningApiClient:
         ).json()
 
     def check_if_error(self, response):
-        if not "metadata" in response:
+        """
+            Checks if Scanning API response has an error
+        """
+        if "metadata" not in response:
             if "message" in response:
                 print(response["message"])
             elif "msg" in response:
@@ -54,15 +60,24 @@ class ScanningApiClient:
 
         return False
 
-    def print_message_leak(self, secret, filename=None):
-        print(
-            "We found a secret from provider"
-            f" {secret['detector']['display_name']}"
-            f" in file {filename}"
-            f" ({secret['matches'][0]['string_matched']})"
-        )
+    def print_message_leak(self, secret, filename: str = None):
+        """
+            Prompt an alert if a leak is found
+        """
+        assert "matches" in secret
+        for match in secret["matches"]:
+            print(
+                "A secret from provider {} has been found in file {} ({})".format(
+                    secret["detector"]["display_name"],
+                    filename,
+                    match["string_matched"],
+                )
+            )
 
-    def scan_file(self, content, filename=None):
+    def scan_file(self, content: str, filename: str = None):
+        """
+            Scan file content
+        """
         response = self._scan_file(content)
         if self.check_if_error(response):
             return True
@@ -77,109 +92,140 @@ class ScanningApiClient:
 class Commit:
     def __init__(self, SHA: str = None) -> None:
         self.SHA = SHA
-        self.patch = ""
-        self.diffs = []
-
-        self.get_patch()
-        self.parse_patch()
 
     def get_patch(self):
+        """
+            Gets the change patch for the commit
+        """
         # Commit case
         if not self.SHA:
-            self.patch = subprocess.check_output(["git", "diff", "--cached"]).decode("utf-8")
-        # Push case
+            return subprocess.check_output(["git", "diff", "--cached"]).decode("utf-8")
+        # Push or CI case
         else:
-            self.patch = subprocess.check_output(["git", "show", self.SHA]).decode("utf-8")
+            return subprocess.check_output(["git", "show", self.SHA]).decode("utf-8")
 
-    def parse_patch(self):
-        # Commit case
-        if not self.SHA:
-            list_diff = self.patch.split("diff --git ")[1:]
-        # Push case
-        else:
-            list_diff = self.patch.split("\ndiff --git ")[1:]
-
-        for diff in list_diff:
-            lines = diff.split("\n")
-            filename = self.get_filename(lines[0])
-            filemode = self.get_filemode(lines[1])
-            content = "\n".join(lines[6:])
-            
-            self.diffs.append({
-                'filename': filename,
-                'filemode': filemode,
-                'content': content
-            })
-
-    def get_filename(self, line):
+    def get_filename(self, line: str):
         assert line.split(" ")[0][2:] == line.split(" ")[1][2:]
         return line.split(" ")[0][2:]
 
-    def get_filemode(self, line):
+    def get_filemode(self, line: str):
+        if line.startswith("index"):
+            return "modified file"
+
         return line.split(" mode ")[0]
 
+    def get_diffs(self, patch: str):
+        """
+            Splits the patches into files and extract content for each one of them
+        """
+        list_diff = re.split(r"^diff --git ", patch, flags=re.MULTILINE)[1:]
+        diffs = []
 
-def get_current_branch(branches):
-    for branch in branches.split("\n"):
-        if branch[0] == "*":
-            return branch.split(" ")[1]
+        for diff in list_diff:
+            lines = diff.split("\n")
+            assert len(lines) > 5
 
-    return None
+            filename = self.get_filename(lines[0])
+            filemode = self.get_filemode(lines[1])
+            content = ""
 
+            if filemode == "modified file":
+                content = "\n".join(lines[5:])
+            else:
+                content = "\n".join(lines[6:])
 
-def pre_commit(client):
-    commit = Commit()
+            diffs.append(
+                {"filename": filename, "filemode": filemode, "content": content}
+            )
 
-    for diff in commit.diffs:
-        has_leaks = client.scan_file(diff["content"], diff["filename"])
-        if has_leaks:
-            sys.exit(1)
+        return diffs
 
-    sys.exit(0)
-
-
-def pre_push(client):
-    branches = subprocess.check_output(["git", "branch"]).decode("utf-8")
-    branch = get_current_branch(branches)
-
-    if not branch:
-        print("Cannot get current branch")
-        sys.exit(1)
-
-    head = subprocess.check_output(["cat", f".git/refs/heads/{branch}"]).decode("utf-8").rstrip()
-    origin = ""
-
-    if os.path.isfile(f".git/refs/remotes/origin/{branch}"):
-        origin = "^"+subprocess.check_output(["cat", f".git/refs/remotes/origin/{branch}"]).decode("utf-8").rstrip()
-
-    list_SHA = subprocess.check_output(["git", "rev-list", head, origin]).decode("utf-8").rstrip().split("\n")
-
-    for SHA in list_SHA:
-        commit = Commit(SHA)
-        
-        for diff in commit.diffs:
+    def scan(self, client):
+        """
+            Scan the patch for each file in the commit
+        """
+        for diff in self.get_diffs(self.get_patch()):
             has_leaks = client.scan_file(diff["content"], diff["filename"])
             if has_leaks:
                 sys.exit(1)
 
-    sys.exit(0)
+
+def get_branch(branches: str = None):
+    """
+        Returns the current git branch
+    """
+    if not branches:
+        branches = subprocess.check_output(["git", "branch"]).decode("utf-8")
+
+    for branch in branches.split("\n"):
+        if branch[0] == "*":
+            return branch.split(" ")[1]
+
+    print("Cannot get current branch")
+    sys.exit(1)
+
+
+def pre_commit():
+    """
+        Scans the commit and exits with code 1 if leak is found
+    """
+    client = ScanningApiClient(os.getenv("GG_SCANNING_API_TOKEN"))
+
+    commit = Commit()
+    commit.scan(client)
+
+
+def pre_push():
+    """
+        Gets the commits SHA for the push and exits with code 1 if leak is found
+    """
+    branch = get_branch()
+
+    head = (
+        subprocess.check_output(["cat", f".git/refs/heads/{branch}"])
+        .decode("utf-8")
+        .rstrip()
+    )
+
+    origin = ""
+    if os.path.isfile(f".git/refs/remotes/origin/{branch}"):
+        origin = (
+            "^"
+            + subprocess.check_output(["cat", f".git/refs/remotes/origin/{branch}"])
+            .decode("utf-8")
+            .rstrip()
+        )
+
+    list_SHA = (
+        subprocess.check_output(["git", "rev-list", head, origin])
+        .decode("utf-8")
+        .rstrip()
+        .split("\n")
+    )
+
+    client = ScanningApiClient(os.getenv("GG_SCANNING_API_TOKEN"))
+
+    for SHA in list_SHA:
+        commit = Commit(SHA)
+        commit.scan(client)
 
 
 def main():
-    assert len(sys.argv) > 2
-    client = ScanningApiClient(os.getenv("GG_SCANNING_API_TOKEN"))
+    assert len(sys.argv) > 1
 
     if sys.argv[1] == "commit":
-        pre_commit(client)
+        pre_commit()
 
     elif sys.argv[1] == "push":
-        pre_push(client)
+        pre_push()
 
     else:
         print("Command not found :", sys.argv)
         sys.exit(1)
 
+    print("No secret has been found !")
+    sys.exit(0)
+
 
 if __name__ == "__main__":
     main()
-
--- setup.py
+++ setup.py
@@ -1,17 +1,17 @@
 from setuptools import setup, find_packages
 
+packages = find_packages()
+
 setup(
-    name='secrets_shield',
-    description='Detect secrets in commit patches',
-    url='https://gitlab.gitguardian.ovh/gg-code/prm/pre-commit.git',
-    version='1.0.0',
-    author='gitguardian',
-    author_email='contact@gitguardian.com',
-    install_requires=['requests==2.21.0'],
-    packages=find_packages(),
+    name="secrets_shield",
+    description="Detect secrets in commit patches",
+    url="https://gitlab.gitguardian.ovh/gg-code/prm/pre-commit.git",
+    version="1.0.0",
+    author="GitGuardian",
+    author_email="dev@gitguardian.com",
+    install_requires=["requests==2.21.0"],
+    packages=packages,
     entry_points={
-        'console_scripts': [
-            'secrets-shield = secrets_shield.secrets_shield:main',
-        ],
+        "console_scripts": ["secrets-shield=secrets_shield.secrets_shield:main"]
     },
 )
