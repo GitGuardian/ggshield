32c70f3ac29c967ca5a6d1d832af4340429e421e
Faramad Seifi <faramad.seifi@gitguardian.com>
2019-06-04 10:19:08 +0200

    Add .gitguardian config file

--- secrets_shield/secrets_shield.py
+++ secrets_shield/secrets_shield.py
@@ -2,24 +2,37 @@
 
 
 import click
+
+from typing import List
+
 from secrets_shield.cli.scan import scan
 from secrets_shield.cli.install import install
 from secrets_shield.cli.token import token
 
 from secrets_shield.client import PublicScanningApiClient
+from secrets_shield.config import load_config
 
 CONTEXT_SETTINGS = dict(help_option_names=["-h", "--help"])
 
 
 @click.group(context_settings=CONTEXT_SETTINGS)
 @click.option("--token", envvar="GITGUARDIAN_TOKEN", help="GitGuardian Token.")
+@click.option("--blacklist", "-b", multiple=True, help="Extend blacklist of detectors.")
+@click.option("--set-blacklist", "-B", multiple=True, help="Set detectors blacklist.")
 @click.pass_context
-def cli(ctx, token):
+def cli(ctx: object, token: str, blacklist: List, set_blacklist: List):
     if not token:
         raise click.ClickException(f"GitGuardian Token is needed.")
 
     ctx.ensure_object(dict)
     ctx.obj["client"] = PublicScanningApiClient(token)
+    ctx.obj["config"] = load_config()
+
+    if set_blacklist:
+        ctx.obj["config"]["blacklist"] = set_blacklist
+
+    elif blacklist:
+        ctx.obj["config"]["blacklist"].update(blacklist)
 
 
 cli.add_command(scan)
