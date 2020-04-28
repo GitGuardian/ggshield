9511d08f1e45d56847b17c9dd9b363319861b96d
João Guerreiro <joao.guerreiro@gitguardian.com>
2020-04-28 10:17:18 +0200

    fix(ggshield): add unit tests

--- ggshield/tests/test_utils.py
+++ ggshield/tests/test_utils.py
@@ -1,4 +1,5 @@
-from ggshield.utils import update_secrets_patch
+from ggshield.pygitguardian import ScanResultSchema
+from ggshield.utils import get_ignore_sha, remove_ignored, update_secrets_patch
 
 
 def test_update_secrets_patch_with_file():
@@ -71,3 +72,45 @@ def test_update_secrets_patch_with_patch():
             "end_index": 54,
         }
     ]
+
+
+def test_remove_ignores():
+    scan_result = ScanResultSchema().load(
+        {
+            "policies": ["File extensions", "Filenames", "Secrets detection"],
+            "policy_breaks": [
+                {
+                    "type": "Facebook Access Tokens",
+                    "policy": "Secrets Detection",
+                    "matches": [
+                        {
+                            "match": "294790898041575",
+                            "index_start": 31,
+                            "index_end": 46,
+                            "type": "client_id",
+                        },
+                        {
+                            "match": "ce3f9f0362bbe5ab01dfc8ee565e4372",
+                            "index_start": 68,
+                            "index_end": 100,
+                            "type": "client_secret",
+                        },
+                    ],
+                }
+            ],
+            "policy_break_count": 1,
+        }
+    )
+
+    assert (
+        get_ignore_sha(scan_result.policy_breaks[0])
+        == "38d9d3464520ed68f18d16e640a4a8b37ef5b17608b455267d100aa487ead314"
+    )
+
+    remove_ignored(
+        scan_result,
+        ["38d9d3464520ed68f18d16e640a4a8b37ef5b17608b455267d100aa487ead314"],
+    )
+
+    assert scan_result.policy_break_count == 0
+    assert len(scan_result.policy_breaks) == 0
