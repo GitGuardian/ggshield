0e4262515fe35ab4b9c30b9c0cdf4a54fa1d54eb
João Guerreiro <joao.guerreiro@gitguardian.com>
2020-05-25 17:11:30 +0200

    fix(ggshield): update deps and version

--- setup.py
+++ setup.py
@@ -1,16 +1,37 @@
+import re
+from os.path import abspath, dirname, join
+
 from setuptools import find_packages, setup
 
 
-packages = find_packages()
+VERSION_RE = re.compile(r"__version__\s*=\s*\"(.*?)\"")
+
+
+def readme():
+    with open(abspath("README.md")) as f:
+        return f.read()
+
+
+current_path = abspath(dirname(__file__))
+
+
+with open(join(current_path, "ggshield", "__init__.py")) as file:
+    content = file.read()
+    result = re.search(VERSION_RE, content)
+    if result is None:
+        raise Exception("could not find package version")
+    __version__ = result.group(1)
 
 setup(
-    name="ggshield",
-    description="Detect policy breaks using GitGuardian's brains",
-    url="https://gitlab.gitguardian.ovh/gg-code/prm/pre-commit.git",
-    version="0.1.0",
     author="GitGuardian",
-    author_email="dev@gitguardian.com",
-    install_requires=["requests==2.22.0", "Click==7.0", "PyYAML==5.1"],
-    packages=packages,
+    author_email="support@gitguardian.com",
+    description=readme(),
     entry_points={"console_scripts": ["ggshield=ggshield.cmd:cli"]},
+    include_package_data=True,
+    install_requires=["requests", "click", "pyyaml"],
+    name="ggshield",
+    packages=find_packages(exclude=["tests"]),
+    url="https://gitlab.gitguardian.ovh/gg-code/prm/pre-commit.git",
+    version=__version__,
+    zip_safe=True,
 )
