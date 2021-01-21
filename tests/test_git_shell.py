b5c7fa7dde7e8ce084324a4a41d6684f79022836
jguerreiro <joao.guerreiro@gitguardian.com>
2021-01-21 11:15:49 +0100

    ci(ggshield): fix broken tests

--- tests/test_git_shell.py
+++ tests/test_git_shell.py
@@ -2,9 +2,7 @@ from ggshield.git_shell import GIT_PATH, is_git_dir, shell
 
 
 def test_git_shell():
-    assert "See 'git help git' for an overview of the system." in shell(
-        [GIT_PATH, "--help"]
-    )
+    assert "usage: git" in shell([GIT_PATH, "help"])
 
 
 def test_is_git_dir():
