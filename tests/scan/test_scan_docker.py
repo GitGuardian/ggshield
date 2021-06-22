857eba3400b5ba0c46a41a1fca3c4197d6bc7a18
jguerreiro <joao.guerreiro@gitguardian.com>
2021-06-22 15:10:52 +0200

    feat(docker): make dockerfile/buildargs specific

--- tests/scan/test_scan_docker.py
+++ tests/scan/test_scan_docker.py
@@ -78,17 +78,17 @@ class TestDockerScan:
         files = get_files_from_docker_archive(DOCKER_EXAMPLE_PATH)
 
         expected_files = {
-            "6f19b02ab98ac5757d206a2f0f5a4741ad82d39b08b948321196988acb9de8b1.json": None,  # noqa: E501
-            "64a345482d74ea1c0699988da4b4fe6cda54a2b0ad5da49853a9739f7a7e5bbc/layer.tar/app/file_one": "Hello, I am the first file!\n",  # noqa: E501
-            "2d185b802fb3c2e6458fe1ac98e027488cd6aedff2e3d05eb030029c1f24d60f/layer.tar/app/file_three.sh": "echo Life is beautiful.\n",  # noqa: E501
-            "2d185b802fb3c2e6458fe1ac98e027488cd6aedff2e3d05eb030029c1f24d60f/layer.tar/app/file_two.py": """print("Hi! I'm the second file but I'm happy.")\n""",  # noqa: E501
+            "Dockerfile or build-args": None,  # noqa: E501
+            DOCKER_EXAMPLE_PATH
+            / "64a345482d74ea1c0699988da4b4fe6cda54a2b0ad5da49853a9739f7a7e5bbc/layer.tar/app/file_one": "Hello, I am the first file!\n",  # noqa: E501
+            DOCKER_EXAMPLE_PATH
+            / "2d185b802fb3c2e6458fe1ac98e027488cd6aedff2e3d05eb030029c1f24d60f/layer.tar/app/file_three.sh": "echo Life is beautiful.\n",  # noqa: E501
+            DOCKER_EXAMPLE_PATH
+            / "2d185b802fb3c2e6458fe1ac98e027488cd6aedff2e3d05eb030029c1f24d60f/layer.tar/app/file_two.py": """print("Hi! I'm the second file but I'm happy.")\n""",  # noqa: E501
         }
 
-        assert set(files.files) == {
-            str(DOCKER_EXAMPLE_PATH / file_path) for file_path in expected_files
-        }
+        assert set(files.files) == {str(file_path) for file_path in expected_files}
 
         for file_path, expected_content in expected_files.items():
-            full_path = DOCKER_EXAMPLE_PATH / file_path
-            file = files.files[str(full_path)]
+            file = files.files[str(file_path)]
             assert expected_content is None or file.document == expected_content
