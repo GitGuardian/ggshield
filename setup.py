2189af3b105daff284b3ea84fd3788186d83d266
jguerreiro <joao.guerreiro@gitguardian.com>
2021-09-23 11:47:51 +0200

    chore(ggshield): drop 3.6 support

--- setup.py
+++ setup.py
@@ -53,7 +53,6 @@ setup(
         "Environment :: Console",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
