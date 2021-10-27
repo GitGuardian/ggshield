787aa88cde7b45d83b13bdf51413691880acb957
jguerreiro <joao.guerreiro@gitguardian.com>
2021-10-27 17:09:04 +0200

    chore(deps): update pygitguardian

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
     entry_points={"console_scripts": ["ggshield=ggshield.cmd:cli_wrapper"]},
     install_requires=[
         "click",
-        "pygitguardian==1.2.2",
+        "pygitguardian==1.3.1",
         "pyyaml",
         "python-dotenv",
         "yaspin",
