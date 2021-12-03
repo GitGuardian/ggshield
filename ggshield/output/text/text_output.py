23b64021a0ba7039c052b870e01ab7d082a9b108
Philippe <philippe@gablain.net>
2021-12-03 14:51:09 +0100

    fix(ci) no secret found no more displayed if secrets found (#147)
    * fix(ci) do not display "no secret found" when secrets are found
    
    Co-authored-by: philippe.gablain@gitguardian.com <philippe.gablain@gitguardian.com>
    Co-authored-by: agateau-gg <aurelien.gateau@gitguardian.com>

--- ggshield/output/text/text_output.py
+++ ggshield/output/text/text_output.py
@@ -52,7 +52,12 @@ class TextHandler(OutputHandler):
                 scan_buf.write(self.process_result(result))
         else:
             if self.verbose:
-                scan_buf.write(no_leak_message())
+                has_results = False
+                if scan.scans:
+                    has_results = any(x.results for x in scan.scans)
+
+                if not has_results:
+                    scan_buf.write(no_leak_message())
 
         if scan.scans:
             for sub_scan in scan.scans:
