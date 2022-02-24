4224efddde40c19da3718630b9610bff3641116b
Jeremy Desanlis <jeremy.desanlis@gitguardian.com>
2022-02-24 14:05:34 +0100

    fix(import): standardize import format

--- ggshield/dev_scan.py
+++ ggshield/dev_scan.py
@@ -10,15 +10,14 @@ from typing import Any, Dict, Iterable, Iterator, List, Optional, Set
 import click
 from pygitguardian import GGClient
 
+from ggshield.config import CPU_COUNT, Cache, Config
+from ggshield.config_types import IgnoredMatch
+from ggshield.git_shell import GIT_PATH, get_list_commit_SHA, is_git_dir, shell
 from ggshield.output import OutputHandler
+from ggshield.path import get_files_from_paths
 from ggshield.scan import Commit, Files, Result, ScanCollection
 from ggshield.text_utils import STYLE, format_text
-
-from .config import CPU_COUNT, Cache, Config
-from .config_types import IgnoredMatch
-from .git_shell import GIT_PATH, get_list_commit_SHA, is_git_dir, shell
-from .path import get_files_from_paths
-from .utils import REGEX_GIT_URL, SupportedScanMode, handle_exception
+from ggshield.utils import REGEX_GIT_URL, SupportedScanMode, handle_exception
 
 
 @contextmanager
