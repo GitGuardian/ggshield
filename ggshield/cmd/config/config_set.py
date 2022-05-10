c3a2a7585b47cc478bc0d9285350fdf245e44da0
Martin Faucheux <martin.faucheux@gitguardian.com>
2022-05-10 14:55:57 +0200

    feat(config): #4001 add config set command

---
+++ ggshield/cmd/config/config_set.py
@@ -0,0 +1,44 @@
+from typing import Optional
+
+import click
+
+from ggshield.core.config import Config
+
+from .constants import FIELD_OPTIONS
+
+
+@click.command()
+@click.argument("field_name", nargs=1, type=click.Choice(FIELD_OPTIONS), required=True)
+@click.argument("value", nargs=1, type=click.STRING, required=True)
+@click.option(
+    "--instance",
+    required=False,
+    type=str,
+    metavar="URL",
+    help="URL of the instance to set the config.",
+)
+@click.pass_context
+def config_set_command(
+    ctx: click.Context, field_name: str, value: str, instance: Optional[str]
+) -> int:
+    """
+    Set the value of a specific field in the auth config.
+    """
+    config: Config = ctx.obj["config"]
+
+    # value type checking must be done per case
+    if field_name == "default_token_lifetime":
+        try:
+            value = int(value)  # type: ignore
+        except ValueError:
+            raise click.ClickException("default_token_lifetime must be an int")
+
+    if instance is None:
+        setattr(config.auth_config, field_name, value)
+
+    else:
+        instance_config = config.auth_config.get_instance(instance)
+        setattr(instance_config, field_name, value)
+
+    config.save()
+    return 0
--- tests/cmd/test_config.py
+++ tests/cmd/test_config.py
@@ -78,3 +78,139 @@ class TestAuthConfigList:
         cmd = ["config", "list"]
         result = cli_fs_runner.invoke(cli, cmd, color=False, catch_exceptions=False)
         return result.exit_code, result.output
+
+
+class TestAuthConfigSet:
+    @pytest.mark.parametrize("value", [0, 365])
+    def test_set_lifetime_default_config_value(self, value, cli_fs_runner):
+        """
+        GIVEN a saved protected config
+        WHEN running the set command with a valid param and no instance specified
+        THEN the default config speficied field value be saved
+        AND other instance configs must not be affected
+        """
+        unchanged_value = 42
+
+        prepare_config(DEFAULT_INSTANCE_URL, default_token_lifetime=unchanged_value)
+        exit_code, output = self.run_cmd(cli_fs_runner, value)
+
+        config = Config()
+        assert config.auth_config.default_token_lifetime == value, output
+        assert (
+            config.auth_config.get_instance(DEFAULT_INSTANCE_URL).default_token_lifetime
+            == unchanged_value
+        ), "The instance config should remain unchanged"
+
+        assert exit_code == 0, output
+        assert output == ""
+
+    @pytest.mark.parametrize("value", [0, 365])
+    def test_set_lifetime_instance_config_value(self, value, cli_fs_runner):
+        """
+        GIVEN a saved protected config
+        WHEN running the set command with a valid param and the instance specified
+        THEN the instance's speficied field value be saved
+        AND other configs must not be affected
+        """
+        unchanged_value = 4
+        default_value = Config().auth_config.default_token_lifetime
+
+        unrelated_instance = "https://some-unreleted-gg-instance.com"
+
+        prepare_config(DEFAULT_INSTANCE_URL, default_token_lifetime=value)
+        prepare_config(unrelated_instance, default_token_lifetime=unchanged_value)
+
+        exit_code, output = self.run_cmd(
+            cli_fs_runner, value, instance_url=DEFAULT_INSTANCE_URL
+        )
+
+        config = Config()
+        assert (
+            config.auth_config.get_instance(DEFAULT_INSTANCE_URL).default_token_lifetime
+            == value
+        ), output
+        assert (
+            config.auth_config.get_instance(unrelated_instance).default_token_lifetime
+            == unchanged_value
+        ), "Unrelated instance config should remain unchanged"
+        assert (
+            config.auth_config.default_token_lifetime == default_value
+        ), "The default auth config should remain unchanged"
+
+        assert exit_code == 0, output
+        assert output == ""
+
+    def test_set_invalid_field_name(self, cli_fs_runner):
+        """
+        GIVEN a saved protected config
+        WHEN running the set command with an invalid field name
+        THEN the command should exit with an error
+        AND other configs must not be affected
+        """
+        default_value = Config().auth_config.default_token_lifetime
+
+        exit_code, output = self.run_cmd(cli_fs_runner, 0, param="invalid_field_name")
+
+        assert exit_code == 2, output
+        expected_output = (
+            "Usage: cli config set [OPTIONS] {default_token_lifetime} VALUE\n"
+            "Try 'cli config set -h' for help.\n\n"
+            "Error: Invalid value for '{default_token_lifetime}': 'invalid_field_name' "
+            "is not 'default_token_lifetime'.\n"
+        )
+        assert output == expected_output
+
+        config = Config()
+        assert (
+            config.auth_config.default_token_lifetime == default_value
+        ), "The instance config should remain unchanged"
+
+    def test_set_lifetime_invalid_value(self, cli_fs_runner):
+        """
+        GIVEN a saved protected config
+        WHEN running the set command with an invalid value
+        THEN the command should exit with an error
+        AND other configs must not be affected
+        """
+        default_value = Config().auth_config.default_token_lifetime
+
+        exit_code, output = self.run_cmd(cli_fs_runner, "wrong_value")
+
+        assert exit_code == 1, output
+        assert output == "Error: default_token_lifetime must be an int\n"
+
+        config = Config()
+        assert (
+            config.auth_config.default_token_lifetime == default_value
+        ), "The instance config should remain unchanged"
+
+    def test_set_lifetime_invalid_instance(self, cli_fs_runner):
+        """
+        GIVEN -
+        WHEN running the set command for an unknown instance
+        THEN the command should exit with an error
+        AND other configs must not be affected
+        """
+        instance_url = "https://some-invalid-gg-instance.com"
+
+        default_value = Config().auth_config.default_token_lifetime
+
+        exit_code, output = self.run_cmd(cli_fs_runner, 0, instance_url=instance_url)
+
+        assert exit_code == 1, output
+        assert output == f"Error: Unknown instance: '{instance_url}'\n"
+
+        config = Config()
+        assert (
+            config.auth_config.default_token_lifetime == default_value
+        ), "The instance config should remain unchanged"
+
+    @staticmethod
+    def run_cmd(
+        cli_fs_runner, value, param="default_token_lifetime", instance_url=None
+    ):
+        cmd = ["config", "set", param, str(value)]
+        if instance_url is not None:
+            cmd.append("--instance=" + instance_url)
+        result = cli_fs_runner.invoke(cli, cmd, color=False, catch_exceptions=False)
+        return result.exit_code, result.output
