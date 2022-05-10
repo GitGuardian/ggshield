302ddaa527f0f21d35a6703fdc3bd2ecc8f08243
Martin Faucheux <martin.faucheux@gitguardian.com>
2022-05-10 14:56:31 +0200

    feat(config): #4001 add config unset command

--- ggshield/cmd/config/__init__.py
+++ ggshield/cmd/config/__init__.py
@@ -2,12 +2,14 @@ import click
 
 from .config_list import config_list_cmd
 from .config_set import config_set_command
+from .config_unset import config_unset_command
 
 
 @click.group(
     commands={
         "list": config_list_cmd,
         "set": config_set_command,
+        "unset": config_unset_command,
     }
 )
 def config_group() -> None:
