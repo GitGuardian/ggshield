bdd9b8a67ca15697fc94a23906fcbad6d95483ed
Martin Faucheux <martin.faucheux@gitguardian.com>
2022-05-10 14:55:57 +0200

    feat(config): #4001 add config set command

--- ggshield/cmd/config/__init__.py
+++ ggshield/cmd/config/__init__.py
@@ -1,11 +1,13 @@
 import click
 
 from .config_list import config_list_cmd
+from .config_set import config_set_command
 
 
 @click.group(
     commands={
         "list": config_list_cmd,
+        "set": config_set_command,
     }
 )
 def config_group() -> None:
