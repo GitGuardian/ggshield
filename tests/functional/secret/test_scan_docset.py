39631c899dc8a826ceb0691ab1c7cb46fa85d54a
Kevin Ottens <kevin.ottens@gitguardian.com>
2022-10-20 11:15:25 +0200

    feat: Add support for the docsets JSONL input

---
+++ tests/functional/secret/test_scan_docset.py
@@ -0,0 +1,41 @@
+from pathlib import Path
+
+from tests.conftest import GG_VALID_TOKEN
+from tests.functional.utils import run_ggshield_scan
+
+
+def test_scan_docset_positive(tmp_path: Path) -> None:
+    test_file = tmp_path / "docset.jsonl"
+    test_file.write_text(
+        '{"type": "github_issue", "id": "https://github.com/foo/bar/issues/2", "documents": ['
+        + '{"id": "https://github.com/foo/bar/issues/2", "content": "issue", "authors": [{"id": "ghost"}]}]}\n'
+        + '{"type": "github_issue", "id": "https://github.com/foo/bar/issues/1", "documents": ['
+        + '{"id": "https://github.com/foo/bar/issues/1", "content": "Token in comment", "authors": [{"id": "ghost"}]},'
+        + '{"id": "https://github.com/foo/bar/issues/1#issuecomment-42",'
+        + f'"content": "apikey = {GG_VALID_TOKEN}",'
+        + ' "authors": [{"id": "ghost2"}]}'
+        + "]}"
+    )
+
+    result = run_ggshield_scan("docset", str(test_file), cwd=tmp_path, expected_code=1)
+    assert "apikey =" in result.stdout
+    assert (
+        "incident has been found in file https://github.com/foo/bar/issues/1#issuecomment-42"
+        in result.stdout
+    )
+
+
+def test_scan_docset_negative(tmp_path: Path) -> None:
+    test_file = tmp_path / "docset.jsonl"
+    test_file.write_text(
+        '{"type": "github_issue", "id": "https://github.com/foo/bar/issues/2", "documents": ['
+        + '{"id": "https://github.com/foo/bar/issues/2", "content": "issue", "authors": [{"id": "ghost"}]}]}\n'
+        + '{"type": "github_issue", "id": "https://github.com/foo/bar/issues/1", "documents": ['
+        + '{"id": "https://github.com/foo/bar/issues/1", "content": "No token", "authors": [{"id": "ghost"}]},'
+        + '{"id": "https://github.com/foo/bar/issues/1#issuecomment-42",'
+        + '"content": "Nothing to see, move along",'
+        + ' "authors": [{"id": "ghost2"}]}'
+        + "]}"
+    )
+
+    run_ggshield_scan("docset", str(test_file), cwd=tmp_path, expected_code=0)
---
+++ tests/unit/scan/test_scan_docset.py
@@ -0,0 +1,23 @@
+from ggshield.cmd.secret.scan.docset import generate_files_from_docsets
+from tests.unit.conftest import DATA_PATH
+
+
+DOCSET_EXAMPLE_PATH = DATA_PATH / "docset-example.jsonl"
+
+
+class TestDocsetScan:
+    def test_generate_files_from_docsets(self):
+        input_file = open(DOCSET_EXAMPLE_PATH, "r")
+        files = list(generate_files_from_docsets(input_file))
+        assert {f.filename for f in files} == {
+            "https://github.com/owner/repo/issues/1",
+            "https://github.com/owner/repo/issues/3",
+            "https://github.com/owner/repo/issues/4",
+            "https://github.com/owner/repo/issues/4#issuecomment-10069",
+            "https://github.com/owner/repo/issues/4#issuecomment-10070",
+            "https://github.com/owner/repo/issues/5",
+            "https://github.com/owner/repo/issues/6",
+            "https://github.com/owner/repo/issues/7",
+            "https://github.com/owner/repo/issues/8",
+            "https://github.com/owner/repo/issues/8#issuecomment-10071",
+        }
