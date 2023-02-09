93a53d100617a6b695587058073b7090aebbfc87
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2023-02-09 16:22:26 +0100

    fix: fix "dubious dir" errors when used with GitLab
    When GitLab clones a repository inside the container, it does so as
    root, in a directory which is not $PWD. When it then runs command inside
    the container, the commands are run as our `app` user. When ggshield
    runs a git command in the checkout directory it fails because files are
    owned by root and the directory has not been marked as safe.
    
    Since we can't know the directory in advance and since all CI servers I
    know of expect the default user in the image to be root (GitHub actively
    discourages using another user in Dockerfile-based GitHub actions, see
    [1]), cave in and remove our `app` user.
    
    [1]: https://docs.github.com/en/actions/creating-actions/dockerfile-support-for-github-actions#user

--- actions/iac/entrypoint.sh
+++ actions/iac/entrypoint.sh
@@ -5,9 +5,6 @@ set -euo pipefail
 # defined in `scripts/action-entrypoint-generator`. To make changes to this
 # file, modify the script and rerun it.
 
-# GitHub overrides $HOME. Set it back to the home directory of our `app` user,
-# otherwise the call to `git config` fails.
-export HOME=/home/app
 
 # Mark the current directory as safe. If we don't do this, git commands fail
 # because the source in $PWD is owned by a different user than our `app` user.
