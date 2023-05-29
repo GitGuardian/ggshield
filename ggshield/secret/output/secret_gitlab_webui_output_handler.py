69df2dc00d617453723d52e7b8d649d13150e5f1
Pierre Lalanne <pierre.lalanne@gitguardian.com>
2023-05-29 14:57:30 +0200

    fix(output): fix GitLabWebUI output handler to integrate ignore known secrets
    The ignore known secret was not available when using ggshield as a pre-receive hook and interacting with GitLab's web UI

--- ggshield/secret/output/secret_gitlab_webui_output_handler.py
+++ ggshield/secret/output/secret_gitlab_webui_output_handler.py
@@ -30,25 +30,46 @@ class SecretGitLabWebUIOutputHandler(SecretOutputHandler):
     See https://docs.gitlab.com/ee/administration/server_hooks.html#custom-error-messages
     """
 
-    def __init__(self, show_secrets: bool = False) -> None:
-        super().__init__(show_secrets=show_secrets, verbose=False)
+    def __init__(
+        self, show_secrets: bool = False, ignore_known_secrets: bool = False
+    ) -> None:
+        super().__init__(
+            show_secrets=show_secrets,
+            verbose=False,
+            ignore_known_secrets=ignore_known_secrets,
+        )
 
-    def _process_scan_impl(self, scan_collection: SecretScanCollection) -> str:
-        results = list(scan_collection.get_all_results())
-        if not results:
+    def _process_scan_impl(self, scan: SecretScanCollection) -> str:
+        results = list(scan.get_all_results())
+        # If no secrets or no new secrets were found
+        if not results or (self.ignore_known_secrets and not scan.has_new_secrets):
             return ""
 
-        policy_breaks = []
+        policy_breaks_to_report = []
         for result in results:
-            policy_breaks += result.scan.policy_breaks
+            if not self.ignore_known_secrets:
+                # Populate GL-HOOK-ERR with all policy breaks found
+                policy_breaks_to_report += result.scan.policy_breaks
+            else:
+                for policy_break in result.scan.policy_breaks:
+                    known = policy_break.known_secret
+                    # Populate GL-HOOK-ERR with only new policy breaks
+                    if not known:
+                        policy_breaks_to_report.append(policy_break)
 
         # Use a set to ensure we do not report duplicate incidents.
         # (can happen when the secret is present in both the old and the new version of
         # the document)
-        formatted_policy_breaks = {format_policy_break(x) for x in policy_breaks}
+        formatted_policy_breaks = {
+            format_policy_break(x) for x in policy_breaks_to_report
+        }
+        break_count = len(formatted_policy_breaks)
 
         break_count = len(formatted_policy_breaks)
-        summary_str = f"{break_count} {pluralize('incident', break_count)}"
+        if self.ignore_known_secrets:
+            summary_str = f"{break_count} new {pluralize('incident', break_count)}"
+        else:
+            summary_str = f"{break_count} {pluralize('incident', break_count)}"
 
         # Putting each policy break on its own line would be more readable, but we can't
         # do this because of a bug in GitLab Web IDE which causes newline characters to
--- tests/unit/secret/output/test_gitlab_webui_output.py
+++ tests/unit/secret/output/test_gitlab_webui_output.py
@@ -1,8 +1,15 @@
+from copy import deepcopy
+
+import pytest
 from pygitguardian.models import Match, PolicyBreak
 
+from ggshield.scan import StringScannable
+from ggshield.secret import Result, Results, SecretScanCollection
 from ggshield.secret.output.secret_gitlab_webui_output_handler import (
+    SecretGitLabWebUIOutputHandler,
     format_policy_break,
 )
+from tests.unit.conftest import _ONE_LINE_AND_MULTILINE_PATCH_CONTENT, TWO_POLICY_BREAKS
 
 
 def test_format_policy_break():
@@ -23,3 +30,98 @@ def test_format_policy_break():
         assert match.match_type in out
         # match value itself must be obfuscated
         assert match.match not in out
+
+
+@pytest.mark.parametrize("ignore_known_secrets", [True, False])
+def test_gitlab_web_ui_output_no_secrets(ignore_known_secrets):
+    """
+    GIVEN a content with no secret
+    WHEN GitLabWebUIOutputHandler manipulates the corresponding scan
+    THEN the error message is empty as expected and the status code is zero
+    """
+    output_handler = SecretGitLabWebUIOutputHandler(
+        show_secrets=True, ignore_known_secrets=ignore_known_secrets
+    )
+    scan = SecretScanCollection(
+        id="scan",
+        type="test",
+        results=Results(results=[], errors=[]),
+    )
+    # call output handler
+    exit_code = output_handler._get_exit_code(
+        SecretScanCollection(
+            id="outer_scan",
+            type="outer_scan",
+            results=Results(results=[], errors=[]),
+            scans=[scan],
+        )
+    )
+    error_msg = output_handler._process_scan_impl(scan=scan)
+
+    assert exit_code == 0
+    assert error_msg == ""
+
+
+@pytest.mark.parametrize("ignore_known_secrets", [True, False])
+@pytest.mark.parametrize(
+    "secrets_types",
+    ["only_new_secrets", "only_known_secrets", "mixed_secrets"],
+)
+def test_gitlab_web_ui_output_ignore_known_secrets(secrets_types, ignore_known_secrets):
+    """
+    GIVEN a content with secrets
+    WHEN GitLabWebUIOutputHandler manipulates the corresponding scan
+    THEN the error message warns about secrets or about only new secrets depending
+    on the ignore_known_secrets parameter
+    """
+    result: Result = Result(
+        StringScannable(content=_ONE_LINE_AND_MULTILINE_PATCH_CONTENT, url="leak.txt"),
+        scan=deepcopy(TWO_POLICY_BREAKS),  # 2 policy breaks
+    )
+
+    all_policy_breaks = result.scan.policy_breaks
+
+    if secrets_types == "only_known_secrets":
+        known_policy_breaks = all_policy_breaks
+        new_policy_breaks = []
+    elif secrets_types == "mixed_secrets":
+        # set only first policy break as known
+        known_policy_breaks = all_policy_breaks[:1]
+        new_policy_breaks = all_policy_breaks[1:]
+    else:
+        known_policy_breaks = []
+        new_policy_breaks = all_policy_breaks
+
+    for index, policy_break in enumerate(known_policy_breaks):
+        policy_break.known_secret = True
+        policy_break.incident_url = (
+            f"https://dashboard.gitguardian.com/workspace/1/incidents/{index}"
+        )
+
+    output_handler = SecretGitLabWebUIOutputHandler(
+        show_secrets=True, ignore_known_secrets=ignore_known_secrets
+    )
+    output = output_handler._process_scan_impl(
+        SecretScanCollection(
+            id="outer_scan",
+            type="outer_scan",
+            results=Results(results=[], errors=[]),
+            scans=[
+                SecretScanCollection(
+                    id="scan",
+                    type="test",
+                    results=Results(
+                        results=[result],
+                        errors=[],
+                    ),
+                )
+            ],
+        )
+    )
+    if ignore_known_secrets:
+        if len(new_policy_breaks):
+            assert f"ggshield found {len(new_policy_breaks)} new" in output
+        else:
+            assert output == ""
+    else:
+        assert f"ggshield found {len(all_policy_breaks)}" in output
