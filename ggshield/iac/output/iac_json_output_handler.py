e19f0f38b709fe274841e41fdd6e797b2b1ba3b8
cyrvan bouchard <cyrvan.bouchard.ext@gitguardian.com>
2023-06-14 15:26:19 +0200

    feat(iac_scan): #IAC-32 add json output for iac scan
    Changelog: skip

--- ggshield/iac/output/iac_json_output_handler.py
+++ ggshield/iac/output/iac_json_output_handler.py
@@ -3,13 +3,18 @@ from typing import Any, Dict, cast
 from ggshield.iac.collection.iac_diff_scan_collection import IaCDiffScanCollection
 from ggshield.iac.collection.iac_path_scan_collection import IaCPathScanCollection
 from ggshield.iac.output.iac_output_handler import IaCOutputHandler
-from ggshield.iac.output.schemas import IaCJSONScanResultSchema
+from ggshield.iac.output.schemas import IaCJSONScanResultSchema, IaCJSONScanDiffResultSchema
 
 
 class IaCJSONOutputHandler(IaCOutputHandler):
     def _process_scan_impl(self, scan: IaCPathScanCollection) -> str:
         scan_dict = IaCJSONOutputHandler.create_scan_dict(scan)
         text = IaCJSONScanResultSchema().dumps(scan_dict)
+        return cast(str, text)
+    
+    def _process_diff_scan_impl(self, scan: IaCDiffScanCollection):
+        scan_dict = IaCJSONOutputHandler.create_diff_scan_dict(scan)
+        text = IaCJSONScanDiffResultSchema().dumps(scan_dict)
         return cast(str, text)
 
     # TODO: Determine a design & implement
@@ -34,3 +39,20 @@ class IaCJSONOutputHandler(IaCOutputHandler):
             scan_dict["total_incidents"] += total_incidents
 
         return scan_dict
+
+    @staticmethod
+    def create_diff_scan_dict(scan: IaCDiffScanCollection):
+        ret = {
+            "added_vulns": [],
+            "persisting_vulns": [],
+            "removed_vulns": [],
+        }
+
+        if scan.result is not None:
+            for incident in scan.result.entities_with_incidents.new:
+                ret["added_vulns"].append(incident)
+            for incident in scan.result.entities_with_incidents.unchanged:
+                ret["persisting_vulns"].append(incident)
+            for incident in scan.result.entities_with_incidents.deleted:
+                ret["removed_vulns"].append(incident)
+        return ret
