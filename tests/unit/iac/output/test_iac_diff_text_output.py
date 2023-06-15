ed52627d9ac8ffe6e938c63c052475abd710b2a3
cyrvan bouchard <cyrvan.bouchard.ext@gitguardian.com>
2023-06-15 21:19:19 +0200

    test(iac_scan): #IAC-32 add test on diff text

--- tests/unit/iac/output/test_iac_diff_text_output.py
+++ tests/unit/iac/output/test_iac_diff_text_output.py
@@ -3,6 +3,7 @@ import re
 from ggshield.iac.output.iac_text_output_handler import IaCTextOutputHandler
 from ggshield.iac.collection.iac_diff_scan_collection import IaCDiffScanCollection
 from ggshield.iac.iac_scan_models import IaCDiffScanResult, IaCDiffScanEntities
+from pygitguardian.iac_models import IaCFileResult, IaCVulnerability
 
 def test_iac_scan_diff_no_vuln_not_verbose():
     """
@@ -24,11 +25,10 @@ def test_iac_scan_diff_no_vuln_not_verbose():
     ))
 
     assert_iac_diff_version_displayed(output)
-    assert_iac_diff_no_incident_message(output)
+    assert_iac_diff_no_new_incident_message(output)
     assert_iac_diff_summary_displayed(output)
 
-
-def test_iac_scan_diff_no_vuln_not_verbose():
+def test_iac_scan_diff_vuln_not_verbose():
     """
     GIVEN a response from the GIM api after a iac scan diff
     WHEN verbose mode is not enabled
@@ -42,13 +42,62 @@ def test_iac_scan_diff_no_vuln_not_verbose():
             type="TYPE",
             iac_engine_version="1.0.0",
             entities_with_incidents=IaCDiffScanEntities(
-                new=[], unchanged=[], deleted=[]
+                new=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ], unchanged=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ], deleted=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ]
             )
         )
     ))
 
     assert_iac_diff_version_displayed(output)
-    assert_iac_diff_summary_displayed(output)
+    assert re.search(r"FILENAME\.tf.*1 new incident detected", output)
+    assert_iac_diff_summary_displayed(output, new=1, unchanged=1, deleted=1)
 
 def test_iac_scan_diff_no_vuln_verbose():
     """
@@ -74,13 +123,89 @@ def test_iac_scan_diff_no_vuln_verbose():
     assert_iac_diff_summary_displayed(output)
 
 
+def test_iac_scan_diff_vuln_verbose():
+    """
+    GIVEN a response from the GIM api after a iac scan diff
+    WHEN verbose mode is enabled
+    THEN output should display version, new incidents, unchanged incidents, deleted incidents, and scan summary
+    """
+    output_handler = IaCTextOutputHandler(True)
+    output = output_handler._process_diff_scan_impl(IaCDiffScanCollection(
+        id="ID",
+        result=IaCDiffScanResult(
+            id="ID",
+            type="TYPE",
+            iac_engine_version="1.0.0",
+            entities_with_incidents=IaCDiffScanEntities(
+                new=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ], unchanged=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ], deleted=[
+                    IaCFileResult(
+                        filename="FILENAME.tf",
+                        incidents=[
+                            IaCVulnerability(
+                                policy="POLICY",
+                                policy_id="POLICY_ID",
+                                line_start=0,
+                                line_end=1,
+                                description="DESCRIPTION",
+                                component="COMPONENT",
+                                severity="SEVERITY",
+                                documentation_url="DOCUMENT_URL"
+                            )
+                        ]
+                    )
+                ]
+            )
+        )
+    ))
+
+    assert_iac_diff_version_displayed(output)
+    assert re.search(r"FILENAME\.tf.*1 new incident detected", output)
+    assert_iac_diff_summary_displayed(output, new=1, unchanged=1, deleted=1)
+
+
 def assert_iac_diff_version_displayed(output: str):
     assert re.search(r"iac-engine-version: \d\.\d{1,3}\.\d", output)
 
 def assert_iac_diff_no_incident_message(output: str):
     assert re.search(r"No incidents have been found", output)
 
-def assert_iac_diff_summary_displayed(output: str):
-    assert re.search(r"\[-\] \d+ incidents? deleted", output)
-    assert re.search(r"\[~\] \d+ incidents? remaining", output)
-    assert re.search(r"\[\+\] \d+ new incidents? detected", output)
+def assert_iac_diff_no_new_incident_message(output: str):
+    assert re.search(r"No new incidents have been found", output)
+
+
+def assert_iac_diff_summary_displayed(output: str, new=0, unchanged=0, deleted=0):
+    assert re.search(r"\[-\] " + str(deleted) + " incidents? deleted", output)
+    assert re.search(r"\[~\] " + str(unchanged) + " incidents? remaining", output)
+    assert re.search(r"\[\+\] " + str(new) + " new incidents? detected", output)
