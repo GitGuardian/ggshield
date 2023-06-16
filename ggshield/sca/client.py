3149bb083cc7fa3cfdb0aeb3f905034af479fbfe
Mathias Millet <mathias.millet@gitguardian.com>
2023-06-16 15:58:38 +0200

    feat(sca): add client with compute_sca_files method

---
+++ ggshield/sca/client.py
@@ -0,0 +1,42 @@
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union, cast
+
+import marshmallow_dataclass
+from pygitguardian.client import GGClient, is_ok, load_detail
+from pygitguardian.models import Base, BaseSchema, Detail, FromDictMixin
+
+
+@dataclass
+class ComputeSCAFilesResult(Base, FromDictMixin):
+    sca_files: List[str]
+    potential_siblings: List[str]
+
+
+ComputeSCAFilesResult.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(ComputeSCAFilesResult, base_schema=BaseSchema)(),
+)
+
+
+class SCAClient:
+    def __init__(self, client: GGClient):
+        self._client = client
+
+    def compute_sca_files(
+        self,
+        touched_files: List[str],
+        extra_headers: Optional[Dict[str, str]] = None,
+    ) -> Union[Detail, ComputeSCAFilesResult]:
+        response = self._client.post(
+            endpoint="sca/compute_sca_files/",
+            data={"touched_files": touched_files},
+            extra_headers=extra_headers,
+        )
+        result: Union[Detail, ComputeSCAFilesResult]
+        if is_ok(response):
+            result = ComputeSCAFilesResult.from_dict(response.json())
+        else:
+            result = load_detail(response)
+
+        result.status_code = response.status_code
+        return result
