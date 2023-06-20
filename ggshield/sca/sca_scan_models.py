ae0bc011485efdf053f688a414afe048706b449c
Xavier Blanchot <xavier.blanchot@gitguardian.com>
2023-06-20 15:01:32 +0200

    feat(sca): #PLC-761 add sca full scan output schema

---
+++ ggshield/sca/sca_scan_models.py
@@ -0,0 +1,80 @@
+from dataclasses import dataclass, field
+from datetime import datetime
+from typing import List, Optional, cast
+
+import marshmallow_dataclass
+from pygitguardian.models import Base, BaseSchema, FromDictMixin
+from typing_extensions import Literal
+
+
+@dataclass
+class SCAVulnerability(Base, FromDictMixin):
+    severity: str
+    summary: str
+    cve_ids: List[str] = field(default_factory=list)
+    created_at: Optional[datetime] = None
+    fixed_version: Optional[str] = None
+
+
+SCAVulnerability.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(SCAVulnerability, base_schema=BaseSchema)(),
+)
+
+SCADependencyType = Literal["direct", "transitive"]
+
+
+@dataclass
+class SCAVulnerablePackageVersion(Base, FromDictMixin):
+    package_full_name: str
+    version: str
+    ecosystem: str
+    dependency_type: Optional[SCADependencyType] = None
+    vulns: List[SCAVulnerability] = field(default_factory=list)
+
+
+SCAVulnerablePackageVersion.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(
+        SCAVulnerablePackageVersion, base_schema=BaseSchema
+    )(),
+)
+
+
+@dataclass
+class SCALocationVulnerability(Base, FromDictMixin):
+    location: str
+    package_vulns: List[SCAVulnerablePackageVersion] = field(default_factory=list)
+
+
+SCALocationVulnerability.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(
+        SCALocationVulnerability, base_schema=BaseSchema
+    )(),
+)
+
+
+@dataclass
+class SCAScanAllOutput(Base, FromDictMixin):
+    scanned_files: List[str] = field(default_factory=list)
+    found_package_vulns: List[SCALocationVulnerability] = field(default_factory=list)
+
+
+SCAScanAllOutput.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(SCAScanAllOutput, base_schema=BaseSchema)(),
+)
+
+
+@dataclass
+class SCAScanDiffOutput(Base, FromDictMixin):
+    scanned_files: List[str] = field(default_factory=list)
+    added_vulns: List[SCALocationVulnerability] = field(default_factory=list)
+    removed_vulns: List[SCALocationVulnerability] = field(default_factory=list)
+
+
+SCAScanDiffOutput.SCHEMA = cast(
+    BaseSchema,
+    marshmallow_dataclass.class_schema(SCAScanDiffOutput, base_schema=BaseSchema)(),
+)
