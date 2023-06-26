0bca9aac797dcae668764a6d7d9212a8fdbc8058
cyrvan bouchard <cyrvan.bouchard.ext@gitguardian.com>
2023-06-26 15:03:45 +0200

    refactor(iac_scan): fix pyright

--- ggshield/core/clickutils/default_command_group.py
+++ ggshield/core/clickutils/default_command_group.py
@@ -8,6 +8,7 @@ import click
 
 class DefaultCommandGroup(click.Group):
     """allow a default command for a group"""
+    default_command: str | None = None
 
     def command(self, *args: Any, **kwargs: Any) -> Any:
         default_command: bool = kwargs.pop("default_command", False)
