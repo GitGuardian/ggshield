0c2afcc9ae87bb22e348a733d03aaeb621cdd2ca
cyrvan bouchard <cyrvan.bouchard.ext@gitguardian.com>
2023-07-11 10:30:15 +0200

    feat(iac_scan_ci): add ci command
    Issue: IAC-120
    Changelog: skip

---
+++ ggshield/cmd/iac/scan/ci.py
@@ -0,0 +1,46 @@
+from pathlib import Path
+from typing import Any, Optional, Sequence
+
+import click
+
+from ggshield.core.text_utils import display_warning
+from ggshield.cmd.iac.scan.all import display_iac_scan_all_result, iac_scan_all
+from ggshield.cmd.iac.scan.diff import display_iac_scan_diff_result, iac_scan_diff
+from ggshield.cmd.iac.scan.iac_scan_common_options import (
+    add_iac_scan_common_options,
+    all_option,
+    directory_argument,
+    update_context,
+)
+
+
+@click.command()
+@add_iac_scan_common_options()
+@all_option
+@directory_argument
+@click.pass_context
+def scan_ci_cmd(
+    ctx: click.Context,
+    exit_zero: bool,
+    minimum_severity: str,
+    ignore_policies: Sequence[str],
+    ignore_paths: Sequence[str],
+    all: bool,
+    directory: Optional[Path] = None,
+    **kwargs: Any,
+) -> int:
+    """
+    Scan in CI for IaC vulnerabilities. By default, it will return vulnerabilities added in the new commits.
+    """
+    display_warning(
+        "This feature is still in beta, its behavior may change in future versions."
+    )
+    if directory is None:
+        directory = Path().resolve()
+    update_context(ctx, exit_zero, minimum_severity, ignore_policies, ignore_paths)
+
+    if all:
+        result = iac_scan_all(ctx, directory)
+        return display_iac_scan_all_result(ctx, directory, result)
+    result = iac_scan_diff(ctx, directory, "HEAD", include_staged=True)
+    return display_iac_scan_diff_result(ctx, directory, result)
