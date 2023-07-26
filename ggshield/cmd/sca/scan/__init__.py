6eb4c68442c5dcde155815220d606cc60425c9f0
Achille Mascia <achille.mascia@gitguardian.com>
2023-07-26 15:33:02 +0200

    feat(sca): Added pre-push command for sca

--- ggshield/cmd/sca/scan/__init__.py
+++ ggshield/cmd/sca/scan/__init__.py
@@ -5,6 +5,7 @@ import click
 from ggshield.cmd.sca.scan.all import scan_all_cmd
 from ggshield.cmd.sca.scan.ci import scan_ci_cmd
 from ggshield.cmd.sca.scan.precommit import scan_pre_commit_cmd
+from ggshield.cmd.sca.scan.prepush import scan_pre_push_cmd
 
 
 @click.group(
@@ -12,6 +13,7 @@ from ggshield.cmd.sca.scan.precommit import scan_pre_commit_cmd
         "pre-commit": scan_pre_commit_cmd,
         "all": scan_all_cmd,
         "ci": scan_ci_cmd,
+        "pre-push": scan_pre_push_cmd,
     }
 )
 @click.pass_context
