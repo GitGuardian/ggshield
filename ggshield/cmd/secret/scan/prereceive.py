d0cbecd6354802c12d0863d1e1aa786b63265839
Téo Lohrer <teo.lohrer@gitguardian.com>
2023-07-31 15:06:12 +0200

    feat(iac): Add pre-receive scan

--- ggshield/cmd/secret/scan/prereceive.py
+++ ggshield/cmd/secret/scan/prereceive.py
@@ -3,7 +3,7 @@ import multiprocessing
 import os
 import re
 import sys
-from typing import Any, List, Optional, Set, Tuple
+from typing import Any, List, Set
 
 import click
 from pygitguardian import GGClient
@@ -14,10 +14,15 @@ from ggshield.cmd.secret.scan.secret_scan_common_options import (
 )
 from ggshield.core.cache import ReadOnlyCache
 from ggshield.core.config import Config
-from ggshield.core.errors import UnexpectedError, handle_exception
-from ggshield.core.git_shell import get_list_commit_SHA, git
+from ggshield.core.errors import handle_exception
+from ggshield.core.git_hooks.prereceive import (
+    BYPASS_MESSAGE,
+    get_breakglass_option,
+    get_prereceive_timeout,
+    parse_stdin,
+)
+from ggshield.core.git_shell import get_list_commit_SHA
 from ggshield.core.text_utils import display_error
-from ggshield.core.utils import EMPTY_SHA, PRERECEIVE_TIMEOUT
 from ggshield.scan import ScanContext, ScanMode
 from ggshield.secret.output import SecretGitLabWebUIOutputHandler, SecretOutputHandler
 from ggshield.secret.output.messages import remediation_message
@@ -26,48 +31,11 @@ from ggshield.secret.repo import scan_commit_range
 
 logger = logging.getLogger(__name__)
 
+
 REMEDIATION_MESSAGE = """  A pre-receive hook set server side prevented you from pushing secrets.
   Since the secret was detected during the push BUT after the commit, you need to:
   1. rewrite the git history making sure to replace the secret with its reference (e.g. environment variable).
   2. push again."""
-
-BYPASS_MESSAGE = """\n     git push -o breakglass"""
-
-
-def get_prereceive_timeout() -> float:
-    try:
-        return float(os.getenv("GITGUARDIAN_TIMEOUT", PRERECEIVE_TIMEOUT))
-    except BaseException as e:
-        display_error(f"Unable to parse GITGUARDIAN_TIMEOUT: {str(e)}")
-        return PRERECEIVE_TIMEOUT
-
-
-def get_breakglass_option() -> bool:
-    """Test all options passed to git for `breakglass`"""
-    raw_option_count = os.getenv("GIT_PUSH_OPTION_COUNT", None)
-    if raw_option_count is not None:
-        option_count = int(raw_option_count)
-        for option in range(option_count):
-            if os.getenv(f"GIT_PUSH_OPTION_{option}", "") == "breakglass":
-                return True
-
-    return False
-
-
-def find_branch_start(commit: str) -> Optional[str]:
-    """
-    Returns the first local-only commit of the branch.
-    Returns None if the branch does not contain any new commit.
-    """
-    # List all ancestors of `commit` which are not in any branches
-    output = git(
-        ["rev-list", commit, "--topo-order", "--reverse", "--not", "--branches"]
-    )
-    ancestors = output.splitlines()
-
-    if ancestors:
-        return ancestors[0]
-    return None
 
 
 def _execute_prereceive(
@@ -108,37 +76,6 @@ def _execute_prereceive(
         sys.exit(handle_exception(error, config.verbose))
 
 
-def parse_stdin() -> Optional[Tuple[str, str]]:
-    """
-    Parse stdin and return the first and last commit to scan,
-    or None if there is nothing to do
-    """
-    prereceive_input = sys.stdin.read().strip()
-    if not prereceive_input:
-        raise UnexpectedError(f"Invalid input arguments: '{prereceive_input}'")
-
-    # TODO There can be more than one line here, for example when pushing multiple
-    # branches. We should support this.
-    line = prereceive_input.splitlines()[0]
-    logger.debug("stdin: %s", line)
-    _old_commit, new_commit, _ = line.split(maxsplit=2)
-
-    if new_commit == EMPTY_SHA:
-        # Deletion event, nothing to do
-        return None
-
-    # ignore _old_commit because in case of a force-push, it is going to be overwritten
-    # and should not be scanned (see #437)
-    start_commit = find_branch_start(new_commit)
-    if start_commit is None:
-        # branch does not contain any new commit
-        old_commit = new_commit
-    else:
-        old_commit = f"{start_commit}~1"
-
-    return (old_commit, new_commit)
-
-
 @click.command()
 @click.argument("prereceive_args", nargs=-1, type=click.UNPROCESSED)
 @click.option(
@@ -166,27 +103,14 @@ def prereceive_cmd(
         )
 
     if get_breakglass_option():
-        click.echo(
-            "SKIP: breakglass detected. Skipping GitGuardian pre-receive hook.",
-            err=True,
-        )
         return 0
 
     before_after = parse_stdin()
     if before_after is None:
-        click.echo("Deletion event or nothing to scan.", err=True)
         return 0
+    else:
+        before, after = before_after
 
-    before, after = before_after
-    if before == after:
-        click.echo(
-            "Pushed branch does not contain any new commit.",
-            err=True,
-        )
-        return 0
-
-    assert before != EMPTY_SHA
-    assert after != EMPTY_SHA
     commit_list = get_list_commit_SHA(
         f"{before}...{after}", max_count=config.max_commits_for_hook + 1
     )
