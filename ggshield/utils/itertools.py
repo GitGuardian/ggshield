68ecf8a4ed50b0d8588f00e9e4b71253e2ed6df5
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2023-08-24 18:32:43 +0200

    refactor(utils): move last utils.py function to utils.itertools

--- ggshield/utils/itertools.py
+++ ggshield/utils/itertools.py
@@ -6,6 +6,7 @@ T = TypeVar("T")
 
 
 def batched(iterable: Iterable[T], batch_size: int) -> Iterable[List[T]]:
+    # This function is a backport of the itertools.batched() function  from Python 3.12
     it = iter(iterable)
     while True:
         batch = list(islice(it, batch_size))
