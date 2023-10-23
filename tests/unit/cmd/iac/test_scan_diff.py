f2ad1be671a7046ed15b328a955121b97783d859
Paul Beslin <paul.beslin.ext@gitguardian.com>
2023-10-23 17:40:50 +0200

    test: add tests for repository url in headers via ScanContext

--- tests/unit/cmd/iac/test_scan_diff.py
+++ tests/unit/cmd/iac/test_scan_diff.py
@@ -191,3 +191,36 @@ def test_iac_scan_diff_ignored_directory(
     assert_invoke_exited_with(result, ExitCode.USAGE_ERROR)
     assert "An ignored file or directory cannot be scanned." in result.stdout
     iac_diff_scan_mock.assert_not_called()
+
+
+@patch("pygitguardian.GGClient.iac_diff_scan")
+def test_iac_scan_diff_context_repository(
+    scan_mock: Mock,
+    tmp_path: Path,
+    cli_fs_runner: CliRunner,
+) -> None:
+    """
+    GIVEN a repository with a remote url
+    WHEN executing a scan diff
+    THEN repository url is sent
+    """
+    local_repo = Repository.create(tmp_path)
+    remote_url = "https://github.com/owner/repository.git"
+    local_repo.git("remote", "add", "origin", remote_url)
+    local_repo.create_commit()
+
+    tracked_file = local_repo.path / "iac_file_single_vulnerability.tf"
+    tracked_file.write_text(_IAC_SINGLE_VULNERABILITY)
+    local_repo.add(tracked_file)
+
+    cli_fs_runner.invoke(
+        cli,
+        ["iac", "scan", "diff", "--ref", "HEAD", "--staged", str(local_repo.path)],
+    )
+
+    scan_mock.assert_called_once()
+    assert any(
+        isinstance(arg, dict)
+        and arg.get("GGShield-Repository-URL") == "github.com/owner/repository"
+        for arg in scan_mock.call_args[0]
+    )
