f4b1ff231cd4a763927bdba80c104dd5e5344a6e
carla martet-doleu <carla.martet.ext@gitguardian.com>
2024-01-04 16:30:16 +0100

    fix(iac): prevent staged files to be scanned on pre-push

--- tests/functional/iac/test_iac_scan_prepush.py
+++ tests/functional/iac/test_iac_scan_prepush.py
@@ -129,3 +129,40 @@ def test_iac_scan_prepush_scan_is_diff_on_new_branch(tmp_path: Path) -> None:
     push_output = local_repo.git("push", "-u", "origin", new_branch)
     # THEN the hook triggers a diff scan
     assert json.loads(push_output.split("\n")[0])["type"] == "diff_scan"
+
+
+@pytest.mark.parametrize(
+    "is_vuln_committed",
+    (True, False),
+)
+def test_iac_scan_prepush_ignore_staged_files(
+    tmp_path: Path, is_vuln_committed: bool
+) -> None:
+    # GIVEN an empty remote repository
+    remote_repo = Repository.create(tmp_path / "remote", bare=True)
+
+    # AND a local clone with a vulnerability
+    local_repo = Repository.clone(remote_repo.path, tmp_path / "local")
+
+    # Add a commit, otherwise git complains the branch does not contain anything
+    local_repo.create_commit()
+
+    # AND ggshield installed as a pre-push hook
+    create_local_hook(tmp_path / "local" / ".git" / "hooks", "iac", "pre-push")
+
+    # AND an IaC file introducing a new vulnerability
+    file = local_repo.path / "vuln.tf"
+    file.write_text(_IAC_SINGLE_VULNERABILITY)
+    local_repo.add("vuln.tf")
+    if is_vuln_committed:
+        local_repo.create_commit()
+
+    # WHEN I try to push
+    # THEN the hook prevents the push if and only if a vuln is committed
+    if not is_vuln_committed:
+        local_repo.push()
+    else:
+        with pytest.raises(CalledProcessError) as exc:
+            local_repo.push()
+            stdout = exc.value.stdout.decode()
+            assert "vuln.tf" in stdout
