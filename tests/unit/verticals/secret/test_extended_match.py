a2f2f487cba339b6c903ae8c8d57661d9a9822ef
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2024-05-27 11:54:14 +0200

    test: add tests for ExtendedMatch

---
+++ tests/unit/verticals/secret/test_extended_match.py
@@ -0,0 +1,67 @@
+from pygitguardian.models import Match
+
+from ggshield.core.lines import get_lines_from_content
+from ggshield.utils.git_shell import Filemode
+from ggshield.verticals.secret.extended_match import ExtendedMatch
+from tests.unit.conftest import _SINGLE_MOVE_PATCH
+
+
+PLAIN_CONTENT = """/*
+01234567890
+*/
+token=ABCD
+
+# Some more content
+"""
+
+
+def test_from_match_for_plain_content():
+    """
+    GIVEN some content
+    AND a match in it
+    WHEN ExtendedMatch.from_match() is called
+    THEN it contains the expected values
+    """
+    lines = get_lines_from_content(PLAIN_CONTENT, Filemode.FILE)
+    start = PLAIN_CONTENT.find("A")
+    # No + 1 here because in a Match object, index_end points to the last character of
+    # the match, not the character after it.
+    end = PLAIN_CONTENT.find("D")
+    match = Match(match="ABCD", match_type="token", index_start=start, index_end=end)
+
+    ex_match = ExtendedMatch.from_match(match, lines, is_patch=False)
+
+    assert ex_match.line_start == 3
+    assert ex_match.line_end == 3
+    # ExtendedMatch.from_match() "hijacks" index_start and index_end: they become
+    # 0-based *columns*, and index_end points to the character *after* the match :/
+    assert ex_match.index_start == 6
+    assert ex_match.index_end == 10
+
+
+def test_from_match_for_a_patch():
+    """
+    GIVEN a match against a patch
+    AND its lines
+    WHEN ExtendedMatch.from_match() is called
+    THEN it contains the expected values
+    """
+
+    # When scanning a patch we send the content after the first "@@" of the patch
+    content = _SINGLE_MOVE_PATCH[_SINGLE_MOVE_PATCH.index("@@") :]
+    lines = get_lines_from_content(content, Filemode.MODIFY)
+    start = 40
+    end = 108
+    match = Match(
+        match=content[start : end + 1],
+        match_type="token",
+        index_start=start,
+        index_end=end,
+    )
+
+    ex_match = ExtendedMatch.from_match(match, lines, is_patch=True)
+
+    assert ex_match.line_start == 150
+    assert ex_match.line_end == 150
+    assert ex_match.index_start == 10
+    assert ex_match.index_end == 79
