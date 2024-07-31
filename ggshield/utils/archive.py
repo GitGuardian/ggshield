2267b0df3e4ef8d1b74881c16781d0d5b545c37f
Aurelien Gateau <aurelien.gateau@gitguardian.com>
2024-07-31 15:30:16 +0200

    fix: make sure `secret scan archive` does not overwrite files
    `shutil.unpack_archive()` can overwrite files outside the destination
    directory when unpacking a malicious tar archive. An attacker could use
    this to overwrite files on the user machine.
    
    This can only happen with tar archives, but the behavior with zip is not
    optimal either: bad files are silently ignored.
    
    With this change, the `secret scan archive` command now aborts if the
    archive contains bad files or bad symbolic links, regardless of whether the
    archive is a tar or a zip. For extra safety, the `secret scan pypi` command
    does the same.

---
+++ ggshield/utils/archive.py
@@ -0,0 +1,164 @@
+import shutil
+import stat
+import tarfile
+from functools import lru_cache
+from pathlib import Path
+from typing import List, Optional, Tuple
+from zipfile import ZipFile, ZipInfo
+
+
+class UnsafeArchive(Exception):
+    """
+    Raised when an archive is considered unsafe.
+
+    An archive is considered unsafe if it contains paths outside the extract directory
+    or symbolic links pointing outside the extract directory.
+    """
+
+    def __init__(self):
+        self.bad_paths: List[Path] = []
+        self.bad_links: List[Tuple[Path, Path]] = []
+
+    def __str__(self) -> str:
+        lines: List[str] = ["Archive contains unsafe files."]
+        if self.bad_paths:
+            lines.append("Paths outside the archive root:")
+            lines.extend(f"- {p}" for p in self.bad_paths)
+        if self.bad_links:
+            lines.append("Links pointing outside the archive root:")
+            lines.extend(f"- {src} -> {target}" for src, target in self.bad_links)
+        return "\n".join(lines)
+
+    def add_bad_path(self, path: Path) -> None:
+        self.bad_paths.append(path)
+
+    def add_bad_link(self, path: Path, target: Path) -> None:
+        self.bad_links.append((path, target))
+
+    def has_fails(self) -> bool:
+        return bool(self.bad_paths) or bool(self.bad_links)
+
+
+def safe_unpack(archive: Path, extract_dir: Path) -> None:
+    """
+    If `archive` is safe, extract it in `extract_dir`. Otherwise, raise `UnsafeArchive`.
+    """
+    check_archive_content(archive)
+
+    # unpack_archive does not know .whl files are zip files
+    archive_format = "zip" if archive.suffix == ".whl" else None
+
+    shutil.unpack_archive(archive, extract_dir, format=archive_format)
+
+
+def check_archive_content(archive: Path) -> None:
+    """
+    Check `archive` safety, raise `UnsafeArchive` if it is unsafe.
+    """
+    if archive.suffix in {".zip", ".whl"}:
+        _check_zip_content(archive)
+    else:
+        _check_tar_content(archive)
+
+
+@lru_cache
+def _archive_root():
+    """
+    Returns a fake archive root, used by _is_bad_path().
+    Can be whatever we want except the root directory, otherwise we won't detect a path
+    outside the archive root.
+    """
+
+    # The call to `resolve()` is required on Windows to include the drive letter.
+    return Path("/some/dir").resolve()
+
+
+def _is_bad_path(path: Path) -> bool:
+    """
+    Verify `path` is not outside the archive root.
+
+    Good paths: `foo`, `foo/../bar`
+    Bad paths: `../foo`, `foo/../../bar`, `/foo/bar`
+    """
+
+    root = _archive_root()
+
+    # Code can be simplified to use Path.is_relative_to() when we move to Python 3.9
+    try:
+        root.joinpath(path).resolve().relative_to(root)
+        return False
+    except ValueError:
+        return True
+
+
+def _is_bad_link(link: Path, target: Path) -> bool:
+    """
+    Given a link pointing to a target, verify it does not point outside the archive
+    root.
+
+    Good link example:
+        link="foo/link", target="../file"
+        Points to "file" in the archive root.
+
+    Bad link example:
+        link="foo/link", target="../../file"
+        Points to "file" in the directory above the archive root.
+
+    Another bad link example:
+        link="foo/link", target="/etc/passwd"
+        Points to "/etc/passwd"
+    """
+
+    # Links are interpreted relative to the directory containing them, so we join
+    # target to the link parent
+    target = link.parent.joinpath(target)
+
+    return _is_bad_path(target)
+
+
+def _check_tar_content(archive: Path) -> None:
+    # This exception is populated with bad paths or bad links if we find some. It's only
+    # raised if we found some.
+    unsafe_archive = UnsafeArchive()
+
+    with tarfile.open(archive) as tf:
+        for info in tf:
+            path = Path(info.name)
+            if _is_bad_path(path):
+                unsafe_archive.add_bad_path(path)
+            elif info.issym() or info.islnk():
+                target = Path(info.linkname)
+                if _is_bad_link(path, target):
+                    unsafe_archive.add_bad_link(path, target)
+
+    if unsafe_archive.has_fails():
+        raise unsafe_archive
+
+
+def _zip_get_symlink_target(zip: ZipFile, info: ZipInfo) -> Optional[Path]:
+    """
+    If `info` points to a symbolic link inside `zip`, then return the link target.
+    If it's a regular file, return None.
+    """
+    if not stat.S_ISLNK(info.external_attr >> 16):
+        return None
+    target = zip.open(info).read().decode()
+    return Path(target)
+
+
+def _check_zip_content(archive: Path) -> None:
+    # This exception is populated with bad paths or bad links if we find some. It's only
+    # raised if we found some.
+    unsafe_archive = UnsafeArchive()
+
+    with ZipFile(archive) as zip:
+        for info in zip.infolist():
+            path = Path(info.filename)
+            if target := _zip_get_symlink_target(zip, info):
+                if _is_bad_link(path, target):
+                    unsafe_archive.add_bad_link(path, target)
+            elif _is_bad_path(path):
+                unsafe_archive.add_bad_path(path)
+
+    if unsafe_archive.has_fails():
+        raise unsafe_archive
---
+++ tests/unit/utils/test_archive.py
@@ -0,0 +1,92 @@
+from pathlib import Path
+from typing import Optional
+from zipfile import ZipFile
+
+import pytest
+
+from ggshield.utils.archive import (
+    UnsafeArchive,
+    _zip_get_symlink_target,
+    check_archive_content,
+    safe_unpack,
+)
+from tests.unit.conftest import DATA_PATH
+
+
+ARCHIVES_PATH = DATA_PATH / "archives"
+BAD_ZIP_PATH = ARCHIVES_PATH / "bad.zip"
+BAD_TAR_PATH = ARCHIVES_PATH / "bad.tar"
+GOOD_ZIP_PATH = ARCHIVES_PATH / "good.zip"
+GOOD_WHL_PATH = ARCHIVES_PATH / "good.whl"
+GOOD_TAR_PATH = ARCHIVES_PATH / "good.tar"
+
+"""
+Both bad.zip and bad.tar have the same content:
+
+./
+./fine
+./subdir/
+./subdir/bad-relative-symlink -> ../../bad-relative
+./subdir/bad-absolute-symlink -> /tmp/bad-absolute
+./subdir/fine-symlink -> ../fine
+../bad-relative
+/tmp/bad-absolute
+"""
+
+
+@pytest.mark.parametrize("archive", [BAD_ZIP_PATH, BAD_TAR_PATH])
+def test_check_archive_content_raises_exception(archive: Path):
+    """
+    GIVEN a bad archive
+    WHEN check_archive_content() is called
+    THEN it raises an exception
+    AND the exception contains the description of what's bad in the archive
+    """
+    with pytest.raises(UnsafeArchive) as exc_info:
+        check_archive_content(archive)
+
+    bad_paths = set(exc_info.value.bad_paths)
+    assert bad_paths == {Path("../bad-relative"), Path("/tmp/bad-absolute")}
+
+    bad_links = set(exc_info.value.bad_links)
+    assert bad_links == {
+        (Path("./subdir/bad-relative-symlink"), Path("../../bad-relative")),
+        (Path("./subdir/bad-absolute-symlink"), Path("/tmp/bad-absolute")),
+    }
+
+    # Basic message check, ensures __str__() is exercised
+    message = str(exc_info.value)
+    assert "Paths outside the archive root:" in message
+    assert "bad-relative" in message
+    assert "Links pointing outside the archive root:" in message
+    assert "bad-absolute-symlink" in message
+
+
+@pytest.mark.parametrize("archive", [GOOD_ZIP_PATH, GOOD_WHL_PATH, GOOD_TAR_PATH])
+def test_check_safe_unpack(tmp_path: Path, archive: Path):
+    """
+    GIVEN a good archive
+    WHEN safe_unpack() is called
+    THEN it unpacks the archive
+    """
+    safe_unpack(archive, tmp_path)
+    unpacked_files = set(p for p in tmp_path.rglob("*") if not p.is_dir())
+    assert unpacked_files == {tmp_path / "fine", tmp_path / "subdir/fine-symlink"}
+
+
+@pytest.mark.parametrize(
+    ("name", "target"),
+    [
+        ("./subdir/fine-symlink", Path("../fine")),
+        ("./fine", None),
+    ],
+)
+def test_zip_get_symlink_target(name: str, target: Optional[Path]):
+    """
+    GIVEN a path inside bad.zip
+    WHEN asked for its symlink target
+    THEN it returns the target if it's a symlink, or None if it's a regular file
+    """
+    with ZipFile(BAD_ZIP_PATH) as zip:
+        info = zip.getinfo(name)
+        assert _zip_get_symlink_target(zip, info) == target
