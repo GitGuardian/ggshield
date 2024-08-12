9e3dd7a026067f5b0996749c30396201c2b1eb8e
Salomé Voltz <salome.voltz@gitguardian.com>
2024-08-12 12:03:36 +0200

    fix(json_output): Use line-based indexing for matches

--- ggshield/verticals/secret/output/secret_json_output_handler.py
+++ ggshield/verticals/secret/output/secret_json_output_handler.py
@@ -4,6 +4,7 @@ from pygitguardian.client import VERSIONS
 from pygitguardian.models import PolicyBreak
 
 from ggshield.core.filter import group_policy_breaks_by_ignore_sha
+from ggshield.verticals.secret.extended_match import ExtendedMatch
 
 from ..secret_scan_collection import Error, Result, SecretScanCollection
 from .schemas import JSONScanCollectionSchema
@@ -109,6 +110,36 @@ class SecretJSONOutputHandler(SecretOutputHandler):
             flattened_dict["incident_url"] = policy_breaks[0].incident_url
 
         for policy_break in policy_breaks:
-            flattened_dict["occurrences"].extend(policy_break.matches)
+            flattened_dict["occurrences"].extend(self.flatten_matches(policy_break))
 
+        return flattened_dict
+
+    def flatten_matches(
+        self,
+        policy_break: PolicyBreak,
+    ) -> List[Dict[str, Any]]:
+        """
+        flatten_policy_breaks_by_line turns a list of policy breaks into a list of
+        tuples of (line, list of matches) mapping a line to a list of matches starting
+        at that line. The list is sorted by line number.
+        """
+        flattened_dict: List[Dict[str, Any]] = []
+        for match in policy_break.matches:
+            assert isinstance(match, ExtendedMatch)
+
+            flattened_match_dict: Dict[str, Any] = {
+                "match": match.match,
+                "type": match.match_type,
+                "line_start": match.line_start,
+                "line_end": match.line_end,
+                "index_start": match.span.column_index_start,
+                "index_end": match.span.column_index_end,
+            }
+            if match.pre_line_start is not None and match.pre_line_end is not None:
+                flattened_match_dict["pre_line_start"] = match.pre_line_start
+                flattened_match_dict["pre_line_end"] = match.pre_line_end
+            if match.post_line_start is not None and match.post_line_end is not None:
+                flattened_match_dict["post_line_start"] = match.post_line_start
+                flattened_match_dict["post_line_end"] = match.post_line_end
+            flattened_dict.append(flattened_match_dict)
         return flattened_dict
